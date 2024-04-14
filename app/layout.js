dbacc2e5fd6c935008e241a0237e99e59abda3b6
Md Yousuf <71249987+yousufislamme@users.noreply.github.com>
2024-04-14 17:02:42 +0600

    Revert "i add a DarkMode theme"

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,6 @@ import Header from "@/components/Header";
 import { NextUIProvider } from "@nextui-org/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,12 +12,9 @@ export default function RootLayout({ children }) {
     <html lang="en" >
       <body className={inter.className}>
         <NextUIProvider>
-          <NextThemesProvider attribute="class" defaultTheme="dark">
-
-            <Header />
-            {children}
-            <Footer />
-          </NextThemesProvider>
+          <Header />
+          {children}
+          <Footer />
         </NextUIProvider>
       </body>
     </html>
