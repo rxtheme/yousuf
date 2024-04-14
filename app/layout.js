cb00587ec06584e8dbfef25a8142ca2b35651e23
yousufislamme <online.okk@gmail.com>
2024-04-14 17:29:32 +0600

    fix path from -b darkMode

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 "use client";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 import { NextUIProvider } from "@nextui-org/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 
 import React from "react";
-import Hero from "@/components/Hero";
+import Hero from "../components/Hero";
 
 
 export default function Home() {
